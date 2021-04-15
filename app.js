285f70c6342eed89ef05be2d02b8ae44839cf50e
mearavindhan <82570857+mearavindhan@users.noreply.github.com>
2021-04-15 13:16:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const path = require("path");
-const Game = require("./models/Game");
 const methodOverride = require("method-override");
-const catchAsyncErr = require("./other/catchAsyncErr")
 const ErrorClass = require("./other/ErrorClass");
-const {gameSchema, commentSchema} = require("./validationSchemas")
-const Comment = require("./models/comments")
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport  = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user");
+const Game = require("./models/Game")
+
+const gameRoutes = require("./routes/gamesRoute");
+const commentRoutes = require("./routes/commentsRoute")
+const userRoutes = require("./routes/usersRoute");
 
 const dbUrl ="mongodb://localhost:27017/game-hub";
 
@@ -32,101 +38,51 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({extended : true}))
-app.use(methodOverride("_method"))
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-const validateGame = (req, res, next) => {
-     
-     let genreObj = req.body.genre; 
-    let genreList = Object.keys(genreObj).map(key => (genreObj[key] === "on") ? key : "") 
-    let platformsObj = req.body.platforms;
-    let platformsList = Object.keys(platformsObj).map(key => (platformsObj[key] === "on") ? key : "")
-    let games = {...req.body.games, genre:genreList, platforms: platformsList}
-    const {error} = gameSchema.validate({games:games});
-     if(error) {
-         const msg = error.details.map(er => er.message).join(",")
-         throw new ErrorClass(msg, 500)
-    } else {
-         next();
+const sessionProps = {
+    secret: "hellosirindlnaln",
+    resave: false,
+    saveUninitialized: true ,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 *24 * 7,
+        maxAge: 1000 * 60 * 60 *24 * 7,
+        sameSite: 'strict'
     }
 }
+app.use(session(sessionProps))
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
 
-const validateComment = (req, res, next) => {
-    const {error} = commentSchema.validate({comment: req.body})
-    if(error) {
-        const msg = error.details.map(er => er.message).join(",")
-        throw new ErrorClass(msg, 500)
-   } else {
-        next();
-   }
-}
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", (req, res) => {
     res.send("Working....")
 })
 
-app.get("/games", catchAsyncErr(async (req, res) => {
-    const games = await Game.find({});
-    res.render("./game/GameIndex", {allgames: games})
-}))
-
-app.get("/games/new", (req, res) => {
-    res.render("./game/newGame");
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error")
+    next();
 })
 
-app.post("/games", validateGame, catchAsyncErr(async(req, res, next) => {
-    if(!req.body.games) throw new ErrorClass("Invalid Game", 500);
-    let genreObj = req.body.genre; 
-    let genreList = Object.keys(genreObj).map(key => (genreObj[key] === "on") ? key : "") 
-    let platformsObj = req.body.platforms;
-    let platformsList = Object.keys(platformsObj).map(key => (platformsObj[key] === "on") ? key : "")
-    let game = new Game({...req.body.games, genre:genreList, platforms: platformsList});
-    await game.save();
-    res.redirect("/games");
-   
-}))
+app.use("/games/:id/comment", commentRoutes)
 
-app.get("/games/:id", catchAsyncErr(async (req, res) => {
-    const game = await Game.findById(req.params.id).populate("comments");
-    console.log(game);
-    res.render("./game/showGame", {game: game})
-}))
+app.use("/games", gameRoutes)
 
-app.delete("/games/:gameId/comments/:id", catchAsyncErr(async (req, res) => {
-    const {gameId, id} = req.params;
-    await Game.findByIdAndUpdate(gameId, {$pull: {comments: id}})
-    await Comment.findByIdAndDelete(req.params.id);
-    res.redirect(`/games/${gameId}`)
-}))
+app.use("/", userRoutes); 
 
-app.post("/games/:id/comment", validateComment, catchAsyncErr(async(req, res) => {
-    const game = await Game.findById(req.params.id)
-    const comment = new Comment(req.body)
-    game.comments.push(comment);
-    await comment.save();
-    await game.save()
-    res.redirect(`/games/${req.params.id}`)
-}))
-
-app.get("/games/:id/edit", catchAsyncErr(async (req, res) => {
-    const game = await Game.findById(req.params.id);
-    res.render("./game/editGame", {game: game,})
-}))
-
-app.delete("/games/:id", catchAsyncErr(async (req, res) => {
-    await Game.findByIdAndDelete(req.params.id)
-    res.redirect("/games");
-}))
-
-app.put("/games/:id", validateGame, catchAsyncErr(async (req,res) => {
-    const id = req.params.id;
-    let genreObj = req.body.genre; 
-    let genreList = Object.keys(genreObj).map(key => (genreObj[key] === "on") ? key : "") 
-    let platformsObj = req.body.platforms;
-    let platformsList = Object.keys(platformsObj).map(key => (platformsObj[key] === "on") ? key : "")
-    await Game.findByIdAndUpdate(id, {...req.body.games, genre: genreList, platforms: platformsList})
-    res.redirect(`/games/${id}`)
-}))
-
+app.get("/search", async (req,res) => {
+    const games = await Game.find({$text: {$search: req.query.query}})
+    res.render("search", {foundGames : games})
+})
 
 app.get("*", (req, res, next) => {
     next(new ErrorClass("Page Not Found", 404) )
